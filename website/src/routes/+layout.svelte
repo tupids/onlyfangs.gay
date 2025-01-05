884d8c8d9c4e2ced083e6d8bc4584c195278460c
Lucas <lucassshanks@gmail.com>
2025-01-05 05:47:35 -0500

    svelte root (#1)
    * svelte root
    
    * nit for svelte

---
+++ website/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+  import '../styles/global.css';
+
+  let { children } = $props();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        enabled: browser,
+      },
+    },
+  });
+</script>
+
+<div class="app">
+  <QueryClientProvider client={queryClient}>
+    <main>
+      {@render children()}
+    </main>
+  </QueryClientProvider>
+</div>
