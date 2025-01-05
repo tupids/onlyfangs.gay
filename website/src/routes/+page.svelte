5b8f6ea8514e2bcab22a24103dbefa168ee5ff5f
Troy Benson <troybensonsa@gmail.com>
2025-01-05 10:01:34 +0000

    add website boilerplate

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<h1>
+
+</h1>
+
+<p>
+    
+</p>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+<style>
+
+</style>
+
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        console.log("Home page mounted");
+    });
+</script>
---
+++ website/src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<h1>
+    About
+</h1>
+
+<p>
+    This is the about page.
+</p>
+
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
+<style>
+
+</style>
